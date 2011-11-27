be3074c9bf9bb309bd72f92d31130402d030655d
Hashcode <hashcode0f@gmail.com>
2011-11-27 03:48:56 -0800

    opengl hacks to enable hwacceleration
    Change-Id: Ib3d9882a802dccf0e063d135511417cd5c227ea9

--- libs/ui/GraphicBuffer.cpp
+++ libs/ui/GraphicBuffer.cpp
@@ -146,6 +146,9 @@ status_t GraphicBuffer::initSize(uint32_t w, uint32_t h, PixelFormat format,
         uint32_t reqUsage)
 {
     GraphicBufferAllocator& allocator = GraphicBufferAllocator::get();
+#ifdef BOARD_EGL_GRALLOC_USAGE_FILTER
+    reqUsage &= ~BOARD_EGL_GRALLOC_USAGE_FILTER;
+#endif
     status_t err = allocator.alloc(w, h, format, reqUsage, &handle, &stride);
     if (err == NO_ERROR) {
         this->width  = w;
