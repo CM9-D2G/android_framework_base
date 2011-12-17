a20e979048f6727ec2efdcbbc864573a05158fd7
Hashcode <hashcode0f@gmail.com>
2011-12-17 15:18:31 -0800

    revert libmedia_helper removal, stub it instead so its built
    Change-Id: I9ffe4649cf7a219fadc9f1f9d770cd4f60b5f819

--- media/libmedia/Android.mk
+++ media/libmedia/Android.mk
@@ -2,16 +2,19 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifneq ($(BOARD_USES_AUDIO_LEGACY),true)
+ifeq ($(BOARD_USES_AUDIO_LEGACY),true)
+LOCAL_SRC_FILES:= \
+    LibMedia_Helper_Dummy.cpp
+else
 LOCAL_SRC_FILES:= \
     AudioParameter.cpp
+endif
 LOCAL_MODULE:= libmedia_helper
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-endif
 
 
 LOCAL_SRC_FILES:= \
