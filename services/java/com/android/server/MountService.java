6ab6669622fe5b943b8cfc47ccd0d9f6515beeeb
Vairavan Srinivasan <vairav@codeaurora.org>
2012-01-24 08:22:14 -0800

    MountService: unlink death notification when unregistering listeners
    Change-Id: I09045cfe67f7da84bc68a50fc5440ea2c6b754e5

--- services/java/com/android/server/MountService.java
+++ services/java/com/android/server/MountService.java
@@ -1294,6 +1294,7 @@ class MountService extends IMountService.Stub
             for(MountServiceBinderListener bl : mListeners) {
                 if (bl.mListener == listener) {
                     mListeners.remove(mListeners.indexOf(bl));
+                    listener.asBinder().unlinkToDeath(bl, 0);
                     return;
                 }
             }
