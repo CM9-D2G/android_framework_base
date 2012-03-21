cc5058b5079227226e51c1205407f16caaa05fc8
Jonathan Bither <jonbither@gmail.com>
2012-03-20 22:17:00 -0400

    See if this fixes noise during bluetooth calls
    FM: missing stream channel
    Change-Id: I429ca6a67ef544180b64bcee41582716f99c5e56

--- media/java/android/media/AudioManager.java
+++ media/java/android/media/AudioManager.java
@@ -168,6 +168,8 @@ public class AudioManager {
     public static final int STREAM_DTMF = AudioSystem.STREAM_DTMF;
     /** @hide The audio stream for text to speech (TTS) */
     public static final int STREAM_TTS = AudioSystem.STREAM_TTS;
+    /** @hide The audio stream for FM Radio */
+    public static final int STREAM_FM = AudioSystem.STREAM_FM;
     /** Number of audio streams */
     /**
      * @deprecated Use AudioSystem.getNumStreamTypes() instead
@@ -186,7 +188,8 @@ public class AudioManager {
         7,  // STREAM_BLUETOOTH_SCO
         7,  // STREAM_SYSTEM_ENFORCED
         11, // STREAM_DTMF
-        11  // STREAM_TTS
+        11, // STREAM_TTS
+        7   // STREAM_FM
     };
 
     /**
