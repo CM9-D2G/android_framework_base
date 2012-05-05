14c52563e264927b2248e181e28f312f1787a22d
Jonathan Bither <jonbither@gmail.com>
2012-05-05 16:24:08 -0400

    Reimplement the motorola keycodes

--- core/java/android/view/KeyEvent.java
+++ core/java/android/view/KeyEvent.java
@@ -599,8 +599,10 @@ public class KeyEvent extends InputEvent implements Parcelable {
     public static final int KEYCODE_BRIGHTNESS_UP   = 215;
     public static final int KEYCODE_BRIGHTNESS_AUTO = 216;
     public static final int KEYCODE_SCREENSHOT      = 217;
+    public static final int KEYCODE_ALT_LOCK      = 218;
+    public static final int KEYCODE_VOICE      = 219;
 
-    private static final int LAST_KEYCODE           = KEYCODE_SCREENSHOT;
+    private static final int LAST_KEYCODE           = KEYCODE_VOICE;
 
     // NOTE: If you add a new keycode here you must also add it to:
     //  isSystem()
@@ -840,6 +842,8 @@ public class KeyEvent extends InputEvent implements Parcelable {
         names.append(KEYCODE_BRIGHTNESS_UP, "KEYCODE_BRIGHTNESS_UP");
         names.append(KEYCODE_BRIGHTNESS_AUTO, "KEYCODE_BRIGHTNESS_AUTO");
         names.append(KEYCODE_SCREENSHOT, "KEYCODE_SCREENSHOT");
+        names.append(KEYCODE_ALT_LOCK, "KEYCODE_ALT_LOCK");
+        names.append(KEYCODE_VOICE, "KEYCODE_VOICE");
     };
 
     // Symbolic names of all metakeys in bit order from least significant to most significant.
