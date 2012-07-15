f01f20a414b186feca6fe440b27e2b4231b3209d
Jonathan Bither <jonbither@gmail.com>
2012-07-14 21:19:45 -0400

    Revert "Fix code problem in writePidDns"
    This reverts commit bdbc115fd7680ddb64ec56837a8df949fc62bede.

--- services/java/com/android/server/ConnectivityService.java
+++ services/java/com/android/server/ConnectivityService.java
@@ -2156,9 +2156,8 @@ private NetworkStateTracker makeWimaxStateTracker() {
             String dnsString = dns.getHostAddress();
             if (changed || !dnsString.equals(SystemProperties.get("net.dns" + j + "." + pid))) {
                 changed = true;
-                SystemProperties.set("net.dns" + j + "." + pid, dns.getHostAddress());
+                SystemProperties.set("net.dns" + j++ + "." + pid, dns.getHostAddress());
             }
-            j++;
         }
         return changed;
     }
